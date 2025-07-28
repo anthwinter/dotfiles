cbba52a7cd095ef414f0037daf82f91f74d42222
Anth Winter <4440999+anthwinter@users.noreply.github.com>
2025-07-28 09:29:59 +0100

    update filtered_items within neo-tree

--- .config/nvim/lua/anth/plugins/neo-tree.lua
+++ .config/nvim/lua/anth/plugins/neo-tree.lua
@@ -1,14 +1,27 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("neo-tree").setup()
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitognored = false,
+				},
+			},
+		})
 
-      vim.keymap.set("n", "<leader>ee", "<cmd>Neotree source=filesystem reveal=true position=current<CR>", { desc = "Toggle file explorer" })
-    end
+		vim.keymap.set(
+			"n",
+			"<leader>ee",
+			"<cmd>Neotree source=filesystem reveal=true position=current<CR>",
+			{ desc = "Toggle file explorer" }
+		)
+	end,
 }
